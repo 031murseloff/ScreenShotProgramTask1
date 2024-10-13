02432118ed59cf6aad3fcec2d35f004bb8c4bf3a
elsanmurselov <elsanmurselov@gmail.com>
2024-10-13 19:24:39 +0400

    first commit

---
+++ ClientSide/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+
+var ip = IPAddress.Parse("192.168.0.248");
+var port = 27001;
+var endPoint = new IPEndPoint(ip, port);
+var client = new TcpClient();
+Console.WriteLine("Client Started");
+
+try
+{
+    client.Connect(endPoint);
+    var stream = client.GetStream();
+
+    if (client.Connected)
+    {
+        while (true)
+        {
+            Bitmap memoryImage = new Bitmap(1920, 1080);
+            using (Graphics memoryGraphics = Graphics.FromImage(memoryImage))
+            {
+                memoryGraphics.CopyFromScreen(0, 0, 0, 0, memoryImage.Size);
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                memoryImage.Save(ms, ImageFormat.Png);
+                byte[] imageBytes = ms.ToArray();
+
+                byte[] lengthBytes = BitConverter.GetBytes(imageBytes.Length);
+                stream.Write(lengthBytes, 0, lengthBytes.Length);
+
+                
+                stream.Write(imageBytes, 0, imageBytes.Length);
+
+                Console.WriteLine("ScreenShot Sent To Server.");
+            }
+
+            Thread.Sleep(10000); 
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    client.Close();
+    Console.WriteLine("Connection Closed");
+}
---
+++ Lesson2ClassPractice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+var ip = IPAddress.Parse("192.168.0.248");
+var port = 27001;
+var endPoint = new IPEndPoint(ip, port);
+var listener = new TcpListener(endPoint);
+listener.Start();
+
+Console.WriteLine($"Server  begin in {ip}:{port}");
+try
+{
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+        var remoteEndpoint = client.Client.RemoteEndPoint as IPEndPoint;
+        var stream = client.GetStream();
+        Directory.CreateDirectory($"{Directory.GetCurrentDirectory()}/{remoteEndpoint!.Address}");
+
+        while (client.Connected)
+        {
+
+            byte[] lengthBytes = new byte[4];
+            long  bytesRead = stream.Read(lengthBytes, 0, lengthBytes.Length);
+            if (bytesRead == 0) break;
+
+            int imageLength = BitConverter.ToInt32(lengthBytes, 0);
+
+            byte[] imageBytes = new byte[imageLength];
+
+            bytesRead = stream.Read(imageBytes, 0, imageLength);
+            if (bytesRead == 0)
+            {
+                break;
+            }
+
+
+
+            string fileName = $"{Directory.GetCurrentDirectory()}/{remoteEndpoint.Address}/Foto_{DateTime.Now:yyyyMMdd_HHmmss}.png";
+            File.WriteAllBytes(fileName, imageBytes);
+
+            Console.WriteLine($"SreenShot  got in {remoteEndpoint.Address}./n it saved as {fileName}");
+        }
+
+        stream.Close();
+        client.Close();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
